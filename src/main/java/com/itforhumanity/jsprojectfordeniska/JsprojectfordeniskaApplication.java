fd3e1dfda2fc7b4b3bbb2f8fec103eef13524968
hrdlifil <filip.hrdlicka@gmail.com>
2018-02-25 18:02:08 +0100

    first comit js

---
+++ src/main/java/com/itforhumanity/jsprojectfordeniska/JsprojectfordeniskaApplication.java
@@ -0,0 +1,12 @@
+package com.itforhumanity.jsprojectfordeniska;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsprojectfordeniskaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsprojectfordeniskaApplication.class, args);
+	}
+}
