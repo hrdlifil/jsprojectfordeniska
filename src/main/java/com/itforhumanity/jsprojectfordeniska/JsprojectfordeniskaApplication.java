5c94d833a2e0f38d1c4911586560e9f04b36561b
hrdlifil <filip.hrdlicka@gmail.com>
2018-02-25 18:08:33 +0100

    first comit devel

--- src/main/java/com/itforhumanity/jsprojectfordeniska/JsprojectfordeniskaApplication.java
+++ src/main/java/com/itforhumanity/jsprojectfordeniska/JsprojectfordeniskaApplication.java
@@ -8,5 +8,7 @@ public class JsprojectfordeniskaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JsprojectfordeniskaApplication.class, args);
+
+		System.out.println("KOKOTE");
 	}
 }
